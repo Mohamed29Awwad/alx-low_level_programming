81f10f187e2f3515c963d09f5a101b1cb665c822
Mohamed29Awwad <mohamedawwad578@gmail.com>
2023-02-21 04:28:12 -0800

    modifying putcher file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,4 @@
-/*
- * File: 0-putchar.c
- *
- */
-
 #include "main.h"
-
-/**
- * main - Prints "_putchar" followed by a new line.
- *
- * Return: Always 0.
- */
 int main(void)
 {
 	_putchar('_');
