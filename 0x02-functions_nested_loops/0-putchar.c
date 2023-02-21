528c30af0e63544bc49a75b41605a6b3d52905bb
Mohamed29Awwad <mohamedawwad578@gmail.com>
2023-02-21 04:25:53 -0800

    Adding putcher task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,24 @@
-#include <main.h>
+/*
+ * File: 0-putchar.c
+ *
+ */
+
+#include "main.h"
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
-	_putchar('e');
+	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 
